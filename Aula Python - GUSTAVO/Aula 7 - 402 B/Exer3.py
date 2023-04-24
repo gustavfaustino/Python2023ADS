b96fd1feb549bb8739a2c27af8c454f02119e115
Gustavo Gabriel <gustavo.fausti107@gmail.com>
2023-04-24 10:39:49 -0300

    Update Exer3.py

--- Aula Python - GUSTAVO/Aula 7 - 402 B/Exer3.py
+++ Aula Python - GUSTAVO/Aula 7 - 402 B/Exer3.py
@@ -1,6 +1,14 @@
 # 3- Faça um programa em Python que leia um valor n, inteiro e positivo, calcule e
 # mostre a seguinte soma:
 # S = 1 + 1/2 + 1/3 + 1/4 +...+ 1/n
+import os
+os.system('cls')
 
 n = int(input('Digite um número positivo e inteiro aqui:  '))
 
+soma = 0
+
+for c in soma (1,n+1):
+  soma +=1/c
+  
+print(f'O resultado da soma é: {soma:.2f}')
