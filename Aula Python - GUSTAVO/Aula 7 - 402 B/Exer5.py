7b176bf4cebf71e03dc24d21d8a7fe52371135e9
Gustavo Gabriel <gustavo.fausti107@gmail.com>
2024-03-20 19:00:59 -0300

    Delete Aula Python - GUSTAVO/Aula 7 - 402 B/Exer5.py

--- Aula Python - GUSTAVO/Aula 7 - 402 B/Exer5.py
+++
@@ -1,7 +0,0 @@
-# 5- Uma pesquisa foi realizada em um grupo de alunos de uma escola. Escreva um
-# programa em Python que leia o sexo e a altura de cada aluno, calcule e mostre a altura
-# média das alunas e dos alunos separadamente. Utilize o comando de repetição que
-# desejar
-
-# Vamos supor que 10 alunos, meninos e meninas, foram intrevistados.
-
